3d665deae141b55a163a0eea0d7325d741f48b54
ClaytonDewey <clay@dryadmedia.com>
2025-11-17 16:11:56 -0600

    style: Start working on navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,43 @@
+import { Icon } from '../svg';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-primary'>
+        <li>
+          <a href='/'>Collections</a>
+        </li>
+        <li>
+          <a href='/'>Men</a>
+        </li>
+        <li>
+          <a href='/'>Women</a>
+        </li>
+        <li>
+          <a href='/'>About</a>
+        </li>
+        <li>
+          <a href='/'>Contact</a>
+        </li>
+      </ul>
+      <ul className='navbar-secondary'>
+        <li>
+          <a href='/'>
+            <Icon name='cart' />
+          </a>
+        </li>
+        <li>
+          <a href='/'>
+            <img
+              src='./images/image-avatar.png'
+              alt='User Avatar'
+              className='avatar'
+            />
+            <div className='sr-only'>User Name</div>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 export default Navbar;
