029205f2677bc8f69db711f2622aed35f9713262
ClaytonDewey <clay@dryadmedia.com>
2025-11-18 12:29:53 -0600

    create: Set up and style product page layout

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 export { default as Button } from './Button';
+export { default as DescriptionContainer } from './DescriptionContainer';
 export { default as Dropdown } from './Dropdown';
 export { default as DropdownButton } from './DropdownButton';
 export { default as DropdownContent } from './DropdownContent';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
+export { default as ImageContainer } from './ImageContainer';
 export { default as Input } from './Input';
 export { default as Navbar } from './Navbar';
+export { default as ProductContainer } from './ProductContainer';
